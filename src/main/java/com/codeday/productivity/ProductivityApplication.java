a0000565231aced6682d2981b195d138f24f3d84
nahomalemu <nahom.worku2015@gmail.com>
2023-07-31 18:43:50 -0700

    Add Goal and User entities - Include controllers for both entities. - Create repos for CRUD operations. - Implement service layers. - Add necessary dependencies. - Establish database connectivity and add environment variable for DB credentials.

--- src/main/java/com/codeday/productivity/ProductivityApplication.java
+++ src/main/java/com/codeday/productivity/ProductivityApplication.java
@@ -2,23 +2,35 @@ package com.codeday.productivity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * ProductivityApplication is the main entry point for the Spring Boot application.
+ * This class bootstraps the Spring Boot application with the help of {@link SpringApplication}.
+ * <p>
+ * The class is also annotated with {@link RestController} which indicates that it can
+ * handle incoming web requests.
+ * </p>
+ * <p>
+ * The {@link SpringBootApplication} annotation indicates that it's a primary configuration class
+ * and also triggers auto-configuration and component scanning.
+ * </p>
+ *
+ * @author [Nahom Alemu]
+ * @version 1.0
+ * @date 26-07-23
+ */
 @SpringBootApplication
 @RestController
 public class ProductivityApplication {
 
-	@GetMapping
-	public String helloWorld() {
-		return "Hello World!";
-	}
-
-	@GetMapping("/message")
-	public String message() {
-		return "Message from Spring Boot";
-	}
-
+	/**
+	 * This is the main method that serves as an entry point for the Java application.
+	 * When the application starts, it triggers the Spring Boot's SpringApplication
+	 * to bootstrap the app.
+	 *
+	 * @param args Arguments passed to the application. Not specifically used in this application.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ProductivityApplication.class, args);
 	}
