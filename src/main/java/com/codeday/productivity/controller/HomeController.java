797e33092039ae1bd6eefbdfe403f8c7ef3ec1eb
nahomalemu <nahom.worku2015@gmail.com>
2023-08-19 10:48:16 -0700

    updated home controller

--- src/main/java/com/codeday/productivity/controller/HomeController.java
+++ src/main/java/com/codeday/productivity/controller/HomeController.java
@@ -1,17 +1,19 @@
 package com.codeday.productivity.controller;
 
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/")
+@RequestMapping(path = "/test", produces = MediaType.TEXT_PLAIN_VALUE)
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String home() {
         return "Welcome to CodeDay Productivity Application!";
     }
 }
+
 
