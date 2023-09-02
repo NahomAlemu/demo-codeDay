d018e7dbdd21a1acf6cc90b0dd4911b4a0dad604
NahomAlemu <55855783+NahomAlemu@users.noreply.github.com>
2023-09-02 14:31:36 -0700

    Update ProductivityApplication.java

--- src/main/java/com/codeday/productivity/ProductivityApplication.java
+++ src/main/java/com/codeday/productivity/ProductivityApplication.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
-@EnableJpaAuditing
 public class ProductivityApplication {
 
 	private static final Logger LOGGER = LogManager.getLogger(ProductivityApplication.class);
